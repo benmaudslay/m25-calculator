0ee8992c86e90fe493d5432f48f3254d6fd53984
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro-9.local>
2021-07-02 12:11:43 +0100

    buttons added

--- src/components/Keys.jsx
+++ src/components/Keys.jsx
@@ -1,11 +1,29 @@
+const keysArray = [
+  { value: "AC", styling: "clear wide" },
+  { value: "/", styling: "operator" },
+  { value: "7", styling: "num" },
+  { value: "8", styling: "num" },
+  { value: "9", styling: "num" },
+  { value: "*", styling: "operator" },
+  { value: "4", styling: "num" },
+  { value: "5", styling: "num" },
+  { value: "6", styling: "num" },
+  { value: "+", styling: "operator" },
+  { value: "1", styling: "num" },
+  { value: "2", styling: "num" },
+  { value: "3", styling: "num" },
+  { value: "-", styling: "operator" },
+  { value: "0", styling: "zero wide" },
+  { value: ".", styling: "num" },
+  { value: "=", styling: "operator" },
+];
+
 export const Keys = () => {
   return (
-    <div>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>4</button>
-      <button>5</button>
+    <div className="keys">
+      {keysArray.map((item, index) => (
+        <button key={index}>{item.value}</button>
+      ))}
     </div>
   );
 };
